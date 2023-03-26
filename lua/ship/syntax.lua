943a15639128ba10969afe87a23ec37d43a4fdc4
System Malt <javierorfo@protonmail.com>
2023-03-26 12:13:48 -0300

    Update syntax.lua

--- lua/ship/syntax.lua
+++ lua/ship/syntax.lua
@@ -1,7 +1,7 @@
--- #####################################################
--- # Maintainer: Javier Orfo                           #
--- # URL:        https://github.com/javiorfo/nvim-ship #
--- #####################################################
+-- #######################################################
+-- # Maintainer: System Malt                             #
+-- # URL:        https://github.com/systemmalt/nvim-ship #
+-- #######################################################
 
 return {
     sync = function()

627ef68cf40ccb271bf3fc9b2763cd51cf4061da
Javier Orfo <javierorfo@protonmail.com>
2023-04-05 15:52:54 -0300

    syntax ship and shipo

--- lua/ship/syntax.lua
+++ lua/ship/syntax.lua
@@ -4,41 +4,43 @@
 -- #####################################################
 
 return {
-    sync = function()
-        vim.cmd("syn keyword shipSection BASE")
-        vim.cmd("syn keyword shipSection HEADERS")
-        vim.cmd("syn keyword shipSection BODY")
-        vim.cmd("syn keyword shipBoolean true false null")
-        vim.cmd("syn keyword shipMethod GET POST PUT DELETE PATCH")
-        vim.cmd("syn keyword shipMethod CONNECT OPTIONS TRACE HEAD")
-        vim.cmd("syn keyword shipInfo STATUS TIME")
-        vim.cmd("syn keyword shipError ERROR")
+    load = function()
+        vim.cmd[[
+            syn keyword shipSection BASE
+            syn keyword shipSection HEADERS
+            syn keyword shipSection BODY
+            syn keyword shipBoolean true false null
+            syn keyword shipMethod  GET POST PUT DELETE PATCH
+            syn keyword shipMethod  CONNECT OPTIONS TRACE HEAD
+            syn keyword shipError   ERROR
 
-        vim.cmd[[syn match shipComment  '#.*$']]
-        vim.cmd[[syn match shipBrackets '[{}]']]
+            syn match shipComment  '#.*$'
+            syn match shipBrackets '[{}]'
 
-        vim.cmd[[syn region shipEnvVar start="{{" end="}}"]]
-        vim.cmd[[syn region shipSection oneline start=/^\s*\~\[/ end=/\]\~/ contains=shipBase,shipHeaders,shipBody]]
-        vim.cmd[[syn region shipString start=+"+ end=+"+ contains=shipEnvVar]]
-        vim.cmd[[syn region shipVariable start="^\w" end="\s"]]
-        vim.cmd[[syn region shipHtmlTag	  start=+<[^/]+ end=+>+]]
-        vim.cmd[[syn region shipHtmlEndTag start=+</+	end=+>+]]
+            syn region shipEnvVar     start="{{" end="}}"
+            syn region shipSection    oneline start=/^\s*\~\[/ end=/\]\~/ contains=shipBase,shipHeaders,shipBody
+            syn region shipString     start=+"+ end=+"+ contains=shipEnvVar
+            syn region shipVariable   start="^\w" end="\s"
+            syn region shipHtmlTag	  start=+<[^/]+ end=+>+
+            syn region shipHtmlEndTag start=+</+	end=+>+
 
-        vim.cmd[[syn match shipJson	     /"\([^"]\|\\\"\)\+"[[:blank:]\r\n]*\:/]]
-    end,
-    hi = function(links)
-        vim.cmd('hi link shipVariable ' .. links.ship_variable)
-        vim.cmd('hi link shipEnvVar ' .. links.ship_env_var)
-        vim.cmd('hi link shipBoolean ' .. links.ship_boolean_null)
-        vim.cmd('hi link shipSection ' .. links.ship_section)
-        vim.cmd('hi link shipMethod ' .. links.ship_method)
-        vim.cmd('hi link shipString ' .. links.ship_string)
-        vim.cmd('hi link shipComment ' .. links.ship_comment)
-        vim.cmd('hi link shipJson ' .. links.ship_json)
-        vim.cmd('hi link shipInfo ' .. links.ship_info)
-        vim.cmd('hi link shipHtmlTag ' .. links.ship_xml_tag)
-        vim.cmd('hi link shipHtmlEndTag ' .. links.ship_xml_tag)
-        vim.cmd('hi link shipBrackets shipSection')
-        vim.cmd('hi link shipError Error')
+            syn match shipJson	     /"\([^"]\|\\\"\)\+"[[:blank:]\r\n]*\:/
+
+            hi link shipComment    Comment
+            hi link shipBoolean    Boolean
+            hi link shipBrackets   shipBoolean
+            hi link shipSection    shipBoolean
+            hi link shipBase       shipSection
+            hi link shipHeaders    shipSection
+            hi link shipBody       shipSection
+            hi link shipString     String
+            hi link shipMethod     Type
+            hi link shipJson       shipMethod
+            hi link shipVariable   Define
+            hi link shipEnvVar     Tag
+            hi link shipHtmlTag    shipMethod
+            hi link shipHtmlEndTag shipMethod
+            hi link shipError      Error
+        ]]
     end
 }
--- syntax/shipo.vim
+++ syntax/shipo.vim
@@ -9,27 +9,4 @@ endif
 
 let b:current_syntax = 'shipo'
     
-syn keyword shipBoolean true false null
-syn keyword shipMethod  GET POST PUT DELETE PATCH
-syn keyword shipMethod  CONNECT OPTIONS TRACE HEAD
-syn keyword shipError   ERROR
-
-syn match shipBrackets '[{}]'
-
-syn region shipString     start=+"+ end=+"+ contains=shipEnvVar
-syn region shipVariable   start="^\w" end="\s"
-syn region shipHtmlTag	  start=+<[^/]+ end=+>+
-syn region shipHtmlEndTag start=+</+	end=+>+
-
-syn match shipJson	     /"\([^"]\|\\\"\)\+"[[:blank:]\r\n]*\:/
-
-hi link shipBoolean    Boolean
-hi link shipBrackets   shipBoolean
-hi link shipBody       shipSection
-hi link shipString     String
-hi link shipMethod     Type
-hi link shipJson       shipMethod
-hi link shipVariable   Define
-hi link shipHtmlTag    shipMethod
-hi link shipHtmlEndTag shipMethod
-hi link shipError      Error
+lua require('ship.syntax').load()

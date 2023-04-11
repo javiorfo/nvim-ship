8839c7f1f2e85876f7665def1f81d771fce66943
Javier Orfo <mrcharkuils@proton.me>
2023-04-11 12:37:19 -0300

    Update ship.lua

--- ftplugin/ship.lua
+++ ftplugin/ship.lua
@@ -1,6 +1,1 @@
--- ######################################################
--- # Maintainer: Mr. Charkuils                          #
--- # URL:        https://github.com/charkuils/nvim-ship #
--- ######################################################
-
 vim.cmd[[autocmd FileChangedShell *.shipo execute]]

039c97a117bef050475a55bb92ad3b992ab21237
Javier Orfo <mrcharkuils@proton.me>
2023-04-11 12:37:51 -0300

    Update core.lua

--- lua/ship/core.lua
+++ lua/ship/core.lua
@@ -1,8 +1,3 @@
--- ######################################################
--- # Maintainer: Mr. Charkuils                          #
--- # URL:        https://github.com/charkuils/nvim-ship #
--- ######################################################
-
 local setup = require'ship'.DEFAULTS
 local util = require'ship.util'
 local Logger = util.logger

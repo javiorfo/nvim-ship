ee778c8db87e7aa29508312d258b35c5be03ee93
Javier Orfo <mrcharkuils@proton.me>
2023-04-11 12:37:07 -0300

    Update shipo.lua

--- ftdetect/shipo.lua
+++ ftdetect/shipo.lua
@@ -1,6 +1,1 @@
--- ######################################################
--- # Maintainer: Mr. Charkuils                          #
--- # URL:        https://github.com/charkuils/nvim-ship #
--- ######################################################
-
 vim.cmd[[autocmd BufRead,BufNewFile *.shipo set filetype=ship]]

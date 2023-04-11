8b8eee07a62d11e703a25e6100074e70213cfd17
Javier Orfo <mrcharkuils@proton.me>
2023-04-11 12:38:23 -0300

    Update util.lua

--- lua/ship/util.lua
+++ lua/ship/util.lua
@@ -1,8 +1,3 @@
--- ######################################################
--- # Maintainer: Mr. Charkuils                          #
--- # URL:        https://github.com/charkuils/nvim-ship #
--- ######################################################
-
 local M = {}
 
 M.sections = {
